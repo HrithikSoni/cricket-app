9f518e9589aabc5445b6c332073b502ef793efc6
Kushagra Srivastava <kushagra@kalpvaig.com>
2023-11-21 11:37:56 +0530

    login page

---
+++ app/routes/screenBundle/AuthBundle.jsx
@@ -0,0 +1,13 @@
+import Login from "../../screens/AuthScreens/Login";
+
+export const LOGIN = {
+  name: "Login",
+  Component: Login,
+};
+export const SIGN_UP = {
+  NAME: "",
+};
+
+const authBundle = [LOGIN, SIGN_UP];
+
+export default authBundle;
--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ParentWrapper from "../components/wrappers/ParentWrapper";
+import MatchCard from "../components/cards/MatchCard";
 
 export default function Home() {
   return (
     <ParentWrapper>
       <Text>Home</Text>
+      <MatchCard />
     </ParentWrapper>
   );
 }
